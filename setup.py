c9b1f72ab603efb6fd65f6b1a1a5c1c4e49bea73
Charley Bodkin <charley@bodkin.me>
2017-06-08 13:24:39 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.rst') as f:
     readme = f.read()
 
 setup(
-    name='django-taggit-tronc',
+    name='django-taggit',
     version='.'.join(str(i) for i in taggit.VERSION),
     description='django-taggit is a reusable Django application for simple tagging.',
     long_description=readme,
